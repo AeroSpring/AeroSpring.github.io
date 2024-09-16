96b03b115f2fee0abe90a2136f2b59e9720ba306
AeroSpring <iQWERTY13@gmail.com>
2024-09-16 11:21:28 +0300

    modified:   events.js 	modified:   index.html

--- events.js
+++ events.js
@@ -8,9 +8,9 @@ AFRAME.registerComponent('markerhandler', {
         animatedMarker.addEventListener('click', function(ev){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
-                const rotation = aEntity.getAttribute('rotation');
-                Object.keys(rotation).forEach((key) => rotation[key] = rotation[key] - 90);
-                aEntity.setAttribute('rotation', rotation);
+                const animationMixer = aEntity.getAttribute('animation-mixer');
+                Object.keys(animationMixer).forEach((key) => animationMixer[key] = "clip: CubeAction; autoplay: false; loop: false");
+                aEntity.setAttribute('animation-mixer', animationMixer);
             }
             console.log('I was clicked at: ', evt.detail.intersection.point);
         });
