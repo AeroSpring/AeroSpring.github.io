808fc10f8f86d25ad6f0f98b7a2d2600aabd02d5
AeroSpring <iQWERTY13@gmail.com>
2024-09-16 21:36:37 +0300

    modified:   events.js 	modified:   index.html

--- events.js
+++ events.js
@@ -5,7 +5,7 @@ AFRAME.registerComponent('markerhandler', {
         const aEntity = document.querySelector("#animated-model");
 
         // every click, we make our model grow in size :)
-        animatedMarker.addEventListener('click', function(ev, _target){
+        animatedMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
 
