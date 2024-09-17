e4951a43479010c2a47e1ebc7b377c2f2409211e
AeroSpring <iQWERTY13@gmail.com>
2024-09-17 08:46:36 +0300

    new file:   archive/Description_20240916 [174 commit].txt 	new file:   archive/events_20240916 [174 commit].js 	new file:   archive/index_20240916 [174 commit].html 	modified:   index.html

---
+++ archive/events_20240916 [174 commit].js
@@ -0,0 +1,27 @@
+AFRAME.registerComponent('markerhandler', {
+
+    init: function() {
+        const animatedMarker = document.querySelector("#animated-marker");
+        const aEntity = document.querySelector("#animated-model");
+
+        //alert('From script');
+
+        animatedMarker.addEventListener('click', function(ev){//, target){
+            alert('Clicked at: ', evt.detail.intersection.point);
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            //if (aEntity && intersectedElement === aEntity) {
+
+                const rotation = aEntity.getAttribute('animation-mixer');
+                var arrayOfStrings = rotation.split(/\s/);
+                Object.keys(rotation).forEach((key) => rotation[key] = "0 0 " + (Number(arrayOfStrings[2]) + 45));
+                aEntity.setAttribute('rotation', rotation);
+
+                const animationMixer = aEntity.getAttribute('animation-mixer');
+                Object.keys(animationMixer).forEach((key) => animationMixer[key] = "clip: ConeAction; autoplay: true; loop: false");
+                aEntity.setAttribute('animation-mixer', animationMixer);
+
+            //}
+            alert('Clicked at: ', evt.detail.intersection.point);
+            //console.log('CClicked at: ', evt.detail.intersection.point);
+        });
+}});
