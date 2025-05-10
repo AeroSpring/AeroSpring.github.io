c6b5790c2b28b2115699850a803eafd4fa1560ec
AeroSpring <iQWERTY13@gmail.com>
2025-05-10 18:26:13 +0300

    20220412 [4]: Cleaned and prepared for PostgreSQL

--- src/webplatform/database.py
+++ src/webplatform/database.py
@@ -1,10 +1,23 @@
-from sqlalchemy     import create_engine
-from sqlalchemy.orm import sessionmaker
-from .settings      import settings
+from sqlalchemy            import create_engine
+from sqlalchemy.orm        import sessionmaker
+from sqlalchemy.engine.url import URL
+#from sqlalchemy.exc       import psycopg2
+# import psycopg2
+from .settings             import settings
 
+# sqlite database connection:
+#engine = create_engine(
+#    settings.database_url,
+#    connect_args = {'check_same_thread': False},
+#)
+
+# postgres database connection:
 engine = create_engine(
-    settings.database_url,
-    connect_args = {'check_same_thread': False},
+#    URL( **settings.connection_data ) # 20230228 after update sqlalchemy to 2.0.4 : don't work
+    'postgresql+psycopg2://' + settings.connection_data['username'] + ':' +
+                               settings.connection_data['password'] + '@' +
+                               settings.connection_data['host']     + '/' +
+                               settings.connection_data['database']
 )
 
 Session = sessionmaker(
--- src/webplatform/settings.py
+++ src/webplatform/settings.py
@@ -2,26 +2,35 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    server_host:    str = '0.0.0.0'
-    server_port:    int = 81
-    database_url:   str = 'sqlite:///./database.sqlite3'
+    server_host     : str  = '0.0.0.0'
+    server_port     : int  = 81
 
-    jwt_secret:     str             # [from .env]
-    jwt_algorithm:  str = 'HS256'
-    jwt_expiration: int = 3600      # [sec]
+#   database_url    : str  = 'sqlite:///./database.sqlite3'
+    connection_data : dict = {
+        # 'drivername': 'postgresql+psycopg2',    # тут можно задать другой драйвер: 'postgres' (указав порт), или MySQL
+        'host'      : 'localhost',
+        'port'      : '5432',
+        'username'  : 'postgres',
+        'password'  : '271828',
+        'database'  : 'notes_db'
+    }
+
+    jwt_secret      : str             # [from .env]
+    jwt_algorithm   : str  = 'HS256'
+    jwt_expiration  : int  = 3600      # [sec]
 
 
 class SettingsLocalServer(BaseSettings):
-    server_host:    str = '127.0.0.1'
-    server_port:    int = 8000
-    database_url:   str = 'sqlite:///./database.sqlite3'
+    server_host     : str  = '127.0.0.1'
+    server_port     : int  = 8000
+    database_url    : str  = 'sqlite:///./database.sqlite3'
 
-    jwt_secret:     str             # [from .env]
-    jwt_algorithm:  str = 'HS256'
-    jwt_expiration: int = 3600      # [sec]
+    jwt_secret      : str             # [from .env]
+    jwt_algorithm   : str  = 'HS256'
+    jwt_expiration  : int  = 3600      # [sec]
 
 
 settings = Settings(
-    _env_file           = '.env',
-    _env_file_encoding  = 'utf-8',
+    _env_file             = '.env',
+    _env_file_encoding    = 'utf-8',
 )
