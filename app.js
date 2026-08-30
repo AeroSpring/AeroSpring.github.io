786b9695f6982efd02750143cfe4afbf3252a271
AeroSpring <iQWERTY13@gmail.com>
2026-08-30 15:32:14 +0300

    modified:   app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ AFRAME.registerComponent('billboard-scale', {
     let currentScale = this.data.baseScale;
     if (distance > this.data.minDist) {
       // Плавное увеличение в зависимости от расстояния (можно настроить формулу)
-      currentScale = this.data.baseScale + (distance * 1.0); 
+      currentScale = this.data.baseScale + (distance * 10.0); 
     //   if (currentScale > this.data.maxScale) {
     //     currentScale = this.data.maxScale; // Ограничиваем потолок, чтобы модель не закрывала полнеба
     //   }
